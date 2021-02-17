1a35418b7a76386d82916cf10a52c0c4650f1510
EvgeniiKatunkin <evgeniikatunkin@gmail.com>
2021-02-17 13:30:17 +0500

    Initial commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    """A class to stor all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (135, 206, 250)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
